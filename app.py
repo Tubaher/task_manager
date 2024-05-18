7fae69c46b05f15bfeecddb2ac87347a6de024c5
Diego Suntaxi <diego.suntaxi@outlook.es>
2024-05-18 10:49:37 -0500

    chore: Update CORS configuration to allow requests from http://localhost:5173

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
-CORS(app)
 
 app.register_blueprint(task_bp, url_prefix="/tasks")
 
+CORS(app, resources={"/*": {"origins": "http://localhost:5173"}})
 
 with app.app_context():
 
@@ -19,4 +19,4 @@ with app.app_context():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=9000)
